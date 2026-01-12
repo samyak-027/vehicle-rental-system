d6b45919258110ca6065461cab2422d756451d3d
samyak-027 <samyak.acharya027@gmail.com>
2026-01-12 12:38:38 +0530

    Added checking details route

--- server-rentals/routes/authRoutes.js
+++ server-rentals/routes/authRoutes.js
@@ -9,17 +9,21 @@ import {
   refreshToken,
   forgotPassword,
   resetPassword,
+  checkEmailAvailability,
+  submitContactForm,
 } from "../controllers/AuthController.js";
 import { authenticateUser } from "../middleware/authUser.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/check-email", checkEmailAvailability);
 router.post("/login", userLogin);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-otp", resendOTP);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
+router.post("/contact", submitContactForm);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
 router.get("/me", authenticateUser, checkSession);
