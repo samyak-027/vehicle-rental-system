be12dc0ab6794ff9fb63b4f431c2e5505257e3e9
samyak-027 <samyak.acharya027@gmail.com>
2026-01-19 21:19:22 +0530

    updated config.js

--- user-frontend/vite.config.js
+++ user-frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss()
+    react()
   ],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
   server: {
     host: '0.0.0.0',
     port: 5174,
